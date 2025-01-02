a485a74fc133cc7f89d8b132339ba25c4faa6d60
Joshua Atendido Bear <joshuabear87@gmail.com>
2025-01-01 16:02:30 -0800

    API updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchCampsites } from './features/campsites/campsitesSlice';
 import { Routes, Route } from 'react-router-dom';
 import { Footer, Header } from './components';
 import { AboutPage, CampsiteDetailPage, CampsitesDirectoryPage, ContactPage, HomePage } from './pages';
+import { fetchPartners } from './features/partners/partnersSlice';
+import { fetchPromotions } from './features/promotions/promotionsSlice';
 import './App.css';
 
 function App() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchCampsites());
+        dispatch(fetchPartners());
+        dispatch(fetchPromotions());
+    }, [dispatch]);
+
     return (
         <div className='App'>
             <Header />
--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,11 +1,32 @@
+import Error from '../../components/Error';
 import { useSelector } from 'react-redux';
 import { Col, Row } from 'reactstrap';
 import CampsiteCard from './CampsiteCard';
 import { selectAllCampsites } from './campsitesSlice';
+import Loading from '../../components/Loading';
 
 const CampsitesList = () => {
     const campsites = useSelector(selectAllCampsites);
     console.log('campsites:', campsites);
+
+    const isLoading = useSelector((state) => state.campsites.isLoading);
+    const errMsg = useSelector((state) => state.campsites.errMsg);
+
+    if (isLoading) {
+        return (
+            <Row>
+                <Loading />
+            </Row>
+        );
+    }
+
+    if (errMsg) {
+        return (
+            <Row>
+                <Error errMsg={errMsg} />
+            </Row>
+        )
+    }
 
     return (
         <Row className='ms-auto'>
